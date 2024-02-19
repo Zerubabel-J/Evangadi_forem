fa5fddf51e63a45fb63fcbfd8accd7c4708c3096
Zerubabel <zerucodes@gmail.com>
2024-02-19 09:41:00 +0300

    registration part done

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require("express");
 const app = express();
 const PORT = 5500;
 
+// db connection
+const dbConnection = require("./db/dbConfig");
 //Express Middleware
 const userRouter = require("./routes/userRoute");
-
+// Json middleware
+app.use(express.json());
 app.use("/api/users", userRouter);
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(err.message);
-  } else {
-    console.log(`Running at port ${PORT}`);
+
+async function start() {
+  try {
+    const resulet = await dbConnection.execute("SELECT 'test' ");
+    await app.listen(PORT);
+    console.log("Connection established");
+    console.log("listening on port", PORT);
+  } catch (error) {
+    console.log(error.message);
   }
-});
+}
+start();
--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,50 @@
-function register(req, res) {
-  res.send("Register");
+// db connection
+const dbConnection = require("../db/dbConfig");
+const bcrypt = require("bcrypt");
+const { StatusCodes } = require("http-status-codes");
+async function register(req, res) {
+  const { username, firstname, lastname, email, password } = req.body;
+  if (!email || !username || !firstname || !lastname || !password) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please provide all require informatio!" });
+  }
+
+  try {
+    const [user] = await dbConnection.query(
+      "select username, userid from user where username = ? or email = ?",
+      [username, email]
+    );
+    if (user.length > 0) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "user already registered" });
+    }
+    if (password.length < 8) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "password must be at least 8 characters" });
+    }
+    // hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(password, salt);
+    await dbConnection.query(
+      "INSERT INTO user (username, firstname, lastname, email, password) VALUES (?,?,?,?,?)",
+      [username, firstname, lastname, email, hashedpassword]
+    );
+    return res.status(StatusCodes.CREATED).json({ msg: "user registered" });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "somthing went wrong, try again" });
+  }
 }
-function login(req, res) {
+
+async function login(req, res) {
   res.send("Login");
 }
-function checkUser(req, res) {
+async function checkUser(req, res) {
   res.send("Check user");
 }
 
