d8d7d5cc038c0a0996c7baf8997112fb30367bc8
Zerubabel <zerucodes@gmail.com>
2024-02-19 11:23:05 +0300

    tokenization done with jwt

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@
 const dbConnection = require("../db/dbConfig");
 const bcrypt = require("bcrypt");
 const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
 async function register(req, res) {
   const { username, firstname, lastname, email, password } = req.body;
   if (!email || !username || !firstname || !lastname || !password) {
@@ -42,10 +43,53 @@ async function register(req, res) {
 }
 
 async function login(req, res) {
-  res.send("Login");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please provide all require informatio!" });
+  }
+
+  try {
+    const [user] = await dbConnection.query(
+      "select username, userid, password from user where email = ?",
+      [email]
+    );
+
+    if (user.length == 0) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "invalid credential" });
+    }
+    // compare password
+    const isMatch = await bcrypt.compare(password, user[0].password);
+    if (!isMatch) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "invalid credential" });
+    }
+    const username = user[0].username;
+    const userid = user[0].userid;
+    const token = jwt.sign({ username, userid }, "secret", {
+      expiresIn: "1d",
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json({ msg: "user login successful", token });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "somthing went wrong, try again" });
+  }
 }
 async function checkUser(req, res) {
-  res.send("Check user");
+  const username = req.user.username;
+  const userid = req.user.userid;
+
+  res.status(StatusCodes.OK).json({ msg: "valid user", username, userid });
+  // res.send("check user");
 }
 
 module.exports = { register, login, checkUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const { register, login, checkUser } = require("../controller/userController");
 // register route
@@ -7,6 +8,6 @@ router.post("/register", register);
 // user login
 router.post("/login", login);
 //check user, to check wh/r the usr is authenticated
-router.get("/check", checkUser);
+router.get("/check", authMiddleware, checkUser);
 
 module.exports = router;
