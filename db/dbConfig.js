9a18c8e56b45c742b41ae753e7903a05f29ae75d
Zerubabel <zerucodes@gmail.com>
2024-02-19 08:04:39 +0300

    database configured and connected

---
+++ db/dbConfig.js
@@ -0,0 +1,19 @@
+const mysql2 = require("mysql2");
+
+const dbConnection = mysql2.createPool({
+  user: "ForemAdmin",
+  database: "EvangadiForem",
+  host: "localhost",
+  password: "Afford@04",
+  connectionLimit: 10,
+});
+
+// dbConnection.execute("SELECT 'test'", (err, result) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+module.exports = dbConnection.promise();
